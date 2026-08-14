d892b9e8221d4b89d154eb256078ae1a930c5680
test <test@tooljet.io>
2026-08-14 19:39:16 +0530

    seed 10 apps + 2 modules

--- server/src/modules/apps/interfaces/IControllerWorkflow.ts
+++
@@ -1,6 +0,0 @@
-import { App as AppEntity } from '@entities/app.entity';
-import { User as UserEntity } from '@entities/user.entity';
-
-export interface IWorkflowController {
-  fetchWorkflows(app: AppEntity, user: UserEntity): Promise<object>;
-}
--- server/src/modules/apps/interfaces/services/IWorkflowService.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '@entities/user.entity';
-
-export interface IWorkflowService {
-  getWorkflows(organizationId: string, user: User): Promise<{ id: string; name: string }[]>;
-}
